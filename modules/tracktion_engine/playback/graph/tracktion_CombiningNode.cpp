d80438dd98d9e24eaee719683e360e19597280fc
kcoul <kieran.coulter@gmail.com>
2025-01-21 19:51:50 -0800

    Patch for Release builds

--- modules/tracktion_engine/playback/graph/tracktion_CombiningNode.cpp
+++ modules/tracktion_engine/playback/graph/tracktion_CombiningNode.cpp
@@ -87,8 +87,9 @@ struct CombiningNode::TimedNode
 
     void process (ProcessContext& pc)
     {
+    #if JUCE_DEBUG
         jassert (hasPrefetched);
-
+    #endif
         // Process all the Nodes
         for (auto n : nodesToProcess)
             n->process (pc.numSamples, pc.referenceSampleRange);
